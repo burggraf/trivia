b5cf9824dbe7d29d5e5a729acc51de5950ad06aa
Mark Burggraf <markb@mantisbible.com>
2024-12-17 10:15:10 -0800

    clean up user/group info

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -2,41 +2,24 @@
   import { createGame } from "$lib/services/gameService.svelte.ts";
   import PageTemplate from "$lib/components/PageTemplate.svelte";
   import { Button } from "$lib/components/ui/button";
-  import {
-    getUser,
-    getCurrentGroup,
-    initializeUser,
-  } from "$lib/services/backend.svelte";
-  import { onMount } from "svelte";
-  import type { User } from "@supabase/supabase-js";
-  import type { Group } from "$lib/services/backend.svelte";
+  import { getUser, getCurrentGroup } from "$lib/services/backend.svelte";
 
-  let user = $state<User | null>(null);
-  let group = $state<Group | null>(null);
-  let loading = $state(true);
+  const user = $derived(getUser());
 
-  onMount(async () => {
-    await initializeUser();
-    user = getUser();
-    group = getCurrentGroup();
-    loading = false;
-  });
+  const group = $derived(getCurrentGroup());
+  let loading = $state(false);
 
   const handleCreateGame = async () => {
-    console.log("user", user);
-    console.log("group", group);
     if (!group) {
       console.log("no current group");
       return;
     }
+    loading = true;
     const { data, error } = await createGame(group.id);
     console.log("data", data);
     console.log("error", error);
+    loading = false;
   };
-  $effect(() => {
-    console.log("user", user);
-    console.log("group", group);
-  });
 </script>
 
 <PageTemplate>
