1894ca001485327507ea53f2ced6fd898e8a1028
Mark Burggraf <markb@mantisbible.com>
2024-12-17 17:09:18 -0800

    added loading state

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -2,23 +2,25 @@
   import { createGame } from "$lib/services/gameService.svelte.ts";
   import PageTemplate from "$lib/components/PageTemplate.svelte";
   import { Button } from "$lib/components/ui/button";
+  import { loadingState } from "$lib/components/loading/loading-state.svelte.ts";
   import { getUser, getCurrentGroup } from "$lib/services/backend.svelte";
 
   const user = $derived(getUser());
-
   const group = $derived(getCurrentGroup());
-  let loading = $state(false);
 
   const handleCreateGame = async () => {
     if (!group) {
       console.log("no current group");
       return;
     }
-    loading = true;
-    const { data, error } = await createGame(group.id);
-    console.log("data", data);
-    console.log("error", error);
-    loading = false;
+    loadingState.show("Creating game...");
+    try {
+      const { data, error } = await createGame(group.id);
+      console.log("data", data);
+      console.log("error", error);
+    } finally {
+      loadingState.hide();
+    }
   };
 </script>
 
@@ -27,13 +29,8 @@
     Test Page
   {/snippet}
   {#snippet Middle()}
-    {#if loading}
-      Loading...
-    {:else}
-      <Button onclick={handleCreateGame}>Create New Game</Button>
-      <br />
-      <br />
-      Welcome: {user?.email}
-    {/if}
+    <Button onclick={handleCreateGame}>Create New Game</Button>
+    <br />
+    <br />
   {/snippet}
 </PageTemplate>
